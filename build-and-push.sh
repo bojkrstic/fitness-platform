fabe073cba17e55bbd74a894e508de14b885f589
Bojan Krstić <bojkrstic@gmail.com>
2026-06-22 15:09:57 +0200

    Added fullscreen button

--- build-and-push.sh
+++ build-and-push.sh
@@ -2,7 +2,7 @@
 set -e
 
 # IMAGE="bojankrlekrstic/fitness-platform:latest"
-IMAGE="bojankrlekrstic/fitness-platform-svc:version1.0.1"
+IMAGE="bojankrlekrstic/fitness-platform-svc:version1.0.2"
 
 echo "Building Docker image: $IMAGE"
 docker build -t "$IMAGE" .
