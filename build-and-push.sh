7db5e58ef38b46067ecaee54ff35f29e2639be9e
Bojan Krstić <bojkrstic@gmail.com>
2026-06-23 14:29:51 +0200

    new version

--- build-and-push.sh
+++ build-and-push.sh
@@ -2,7 +2,7 @@
 set -e
 
 IMAGE="${DOCKER_IMAGE:-bojankrlekrstic/fitness-platform-svc}"
-TAG="${DOCKER_TAG:-version1.0.3}"
+TAG="${DOCKER_TAG:-version1.0.4}"
 FULL_IMAGE="${IMAGE}:${TAG}"
 
 echo "Building Docker image: $FULL_IMAGE"
