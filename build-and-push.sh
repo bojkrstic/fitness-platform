7588360b139142c3b925f0c3a8de948c7360d9d2
Bojan Krstić <bojkrstic@gmail.com>
2026-06-29 15:05:43 +0200

    New migrations

--- build-and-push.sh
+++ build-and-push.sh
@@ -2,7 +2,7 @@
 set -e
 
 IMAGE="${DOCKER_IMAGE:-bojankrlekrstic/fitness-platform-svc}"
-TAG="${DOCKER_TAG:-version1.0.6}"
+TAG="${DOCKER_TAG:-version1.0.7}"
 FULL_IMAGE="${IMAGE}:${TAG}"
 
 echo "Building Docker image: $FULL_IMAGE"
